a6a83b92e5e6cd3a4b26ac06263e1f6cd9ff3de0
Candela Landi <landicandela01@gmail.com>
2024-12-06 19:40:43 -0300

    finish

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const CharactersByHero = ({ alter_ego,characters }) => {
+const CharactersByHero = ({ alter_ego, characters }) => {
   if (alter_ego === characters) return <></>;
 
-  return <p>{ characters }</p>
-
-}
-
+  return <p>{characters}</p>;
+};
 
 export const HeroCard = ({
   id,
@@ -20,26 +18,27 @@ export const HeroCard = ({
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
 
   return (
-    <div className="col">
+    <div className="col animate__animated animate__fadeIn">
       <div className="card">
         <div className="row no-gutters">
-          <div className="col-4"></div>
-          <img src={heroImageUrl} className="card-img" alt={superhero} />
+          <div className="col-4 ">
+            <img src={heroImageUrl} className="card-img" alt={superhero} />
+          </div>
 
-          <div className="card-body">
-            <h5 className="card-title">{superhero}</h5>
-            <p className="card-text">{alter_ego}</p>
-{/*             {
+          <div className="col-8">
+            <div className="card-body">
+              <h5 className="card-title">{superhero}</h5>
+              <p className="card-text">{alter_ego}</p>
+              {/*             {
             alter_ego !== characters && <p>{characters}</p>
             } */}
-            <CharactersByHero alter_ego={alter_ego} characters={characters} />
-            
-            <p className="card-text">
-              <small className="text-muted">{first_appearance}</small>
-            </p>
-            <Link to={`/heroes/${id}`} >
-            Más...
-            </Link>
+              <CharactersByHero alter_ego={alter_ego} characters={characters} />
+
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+              <Link to={`/hero/${id}`}>Más...</Link>
+            </div>
           </div>
         </div>
       </div>
